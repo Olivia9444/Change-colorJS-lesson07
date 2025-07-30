f2837e0b1158ded6704b3abd82202d01e5c55826
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-07-30 01:11:40 +0000

    js-updates

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+});
